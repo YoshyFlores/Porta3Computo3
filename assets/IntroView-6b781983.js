886d6a5388bb44021259bc6911a2e3b2f8199ca9
YoshyFlores <yoshy.floresg17@gmail.com>
2023-06-09 14:38:00 -0600

    Add Dist

---
+++ assets/IntroView-6b781983.js
@@ -0,0 +1,34 @@
+import{B as o}from"./InvesComponent-4120ad3c.js";import{o as r,c as s,a as e,b as a}from"./index-01d5cb7f.js";const c={class:"container text-center"},d={class:"container text-center"},l=e("div",{class:"p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end"},[e("h2",null,"Introducción")],-1),m=e("br",null,null,-1),p={class:"col-lg-6 mx-auto"},u={__name:"Introduccion",setup(n){return(t,i)=>(r(),s("div",c,[e("div",d,[l,m,e("div",p,[a(o,{text:`En el presente portafolio se brindará información de todo lo aprendido\r
+        durante el primer computo de la materia de Programación IV, en este se\r
+        mostrarán capturas y fotos de las actividades y los apuntes tomados en\r
+        clases. Este primer computo estuvo lleno de muchos retos, ya que\r
+        iniciamos a trabajar con VueJS, así mismo involucramos Vite y Bootstrap.\r
+        Al inicio considero que muchos aspectos de trabajar tanto con JavaScript\r
+        y VueJS era complicados, ya que es una estructura un poco peculiar, pero\r
+        mediante las explicación e investigaciones externas se logro entender de\r
+        cierta forma todos los elementos y contenidos que se van presentando en\r
+        las clases. Se han realizados diversas actividades, en las practicas se\r
+        ha estado trabajando en una página, en la cual se crearon componentes y\r
+        vistas respectivas para conformar la página web, así mismo se realizo\r
+        filtrado. Así mismo, se trabajó con Bootstrap, este es un marco de desarrollo web\r
+        que permite dar estilo a nuestras páginas web, anteriormente ya había\r
+        trabajado con este marco de interfaz, pero en cosas pequeñas y\r
+        sencillas, así que se volvió un reto poder incorporar directamente un\r
+        template en vue.js pero mediante la indagación y la practica se logro\r
+        trabajar con este. Durante las clases prácticas se desarrollaron muchas actividades,\r
+      creamos una pequeña página web, así mismo aprendimos como consumir una API y realizamos \r
+      filtrado. Todos los cambios y actividades que realizamos las subimos a un repositorio en Github y\r
+      posteriormente lo compartimos como evidencia que se ha trabajado en clases.\r
+      Esencialmente, lo que se ha recabado en este portafolio es una parte de todo lo visto y aprendido\r
+      en clases.`}),a(o,{text:`Por otra parte, en el computo dos, se continuo trabajando con nuestro portafolio, pero de igual forma\r
+      se desarrollaron diversos temas que se presentan en dicho trabajo, se podrá observar que hemos trabajado con \r
+      laravel así como también la utilización de mysql para la creación de algunas base de datos. Se ha trabajado con \r
+      PHP, extrayendo lo más importante de este lenguaje de programación. Básicamente, se ha plasmado la parte esencial \r
+      de todo lo realizado tanto en el primer como en el segundo computo con el objetivo de mostrar lo que se ha\r
+      aprendido y estudiado. Cabe mencionar que se comparte el enlace al repositorio de cada computo, donde se evidencian\r
+      todas las prácticas y actividades realizadas. `}),a(o,{text:`En el computo tres se pretende comprender como funcionan las API'S, así mismo se aprenderá como crearlas.\r
+      Las API permiten que una aplicación extraiga archivos o datos preexistentes dentro de un software \r
+      y los use en otro programa o en uno de sus otros niveles. Las prácticas que se desarrollaron durante las clases de iban \r
+      orientadas al desarrollo de API'S. De igual forma, se realizarán prácticas de como consumir las API'S.Finalmente, en este portafolio de la materia de Programación Computacional III, se recaban todos los contenidos\r
+      impartidos a lo largo de los computos, con el objetivo de dar a conocer una pequeña parte de lo aprendido en \r
+      las clases, evidenciandolas en las prácticas desarrolladas.`})])])]))}},_={__name:"IntroView",setup(n){return(t,i)=>(r(),s("main",null,[a(u)]))}};export{_ as default};
---
+++ assets/pp-b7488d25.js
@@ -0,0 +1,1 @@
+const s="/Porta3Computo3/assets/p-17211d9d.jpeg",o="/Porta3Computo3/assets/pp-b7db49c0.jpeg";export{s as _,o as a};
